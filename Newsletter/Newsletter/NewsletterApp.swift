e843b31f9a82b6c2ed662d791a5f997279644001
Alexander677 <alexanderhristichenko@yandex.ru>
2023-06-02 18:13:27 +0300

    add the initial project

---
+++ Newsletter/Newsletter/NewsletterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsletterApp.swift
+//  Newsletter
+//
+//  Created by Христиченко Александр on 2023-06-02.
+//
+
+import SwiftUI
+
+@main
+struct NewsletterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
